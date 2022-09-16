9e62c205e1a66a03b80b1c782104c77468e6c5c6
Pratheesh P S <pratheeshps.me@gmail.com>
2022-09-16 12:38:53 +0000

    Update 5 files, Integrated full functionlaity

--- src/utils.js
+++ src/utils.js
@@ -14,33 +14,38 @@ export const generatePasswordString = ({
   lowercase
 }) => {
   let passkeyArray = [];
-
+  let randomString = [];
+  
+  if (lowercase) {
+    const lowerLetters = generateAsciArray(97, 122);
+    randomString.push(lowerLetters[Math.floor(Math.random() * lowerLetters.length)]);
+    passkeyArray = [...passkeyArray, ...lowerLetters];
+  }
+  
   if (symbols) {
     const symbolsArray = generateAsciArray(33, 47)
       .concat(generateAsciArray(58, 64))
       .concat(generateAsciArray(91, 96))
       .concat(generateAsciArray(123, 126));
-
+    randomString.push(symbolsArray[Math.floor(Math.random() * symbolsArray.length)]);
     passkeyArray = [...passkeyArray, ...symbolsArray];
   }
 
   if (numbers) {
     const numbersArray = generateAsciArray(48, 57);
+    randomString.push(numbersArray[Math.floor(Math.random() * numbersArray.length)]);
     passkeyArray = [...passkeyArray, ...numbersArray];
   }
 
   if (uppercase) {
     const upperLetters = generateAsciArray(65, 90);
+    randomString.push(upperLetters[Math.floor(Math.random() * upperLetters.length)]);
     passkeyArray = [...passkeyArray, ...upperLetters];
   }
 
-  if (lowercase) {
-    const lowerLetters = generateAsciArray(97, 122);
-    passkeyArray = [...passkeyArray, ...lowerLetters];
-  }
-
+  
   const shuffledAsciArray = passkeyArray.sort(() => Math.random() - 0.5);
-  const finalstring = shuffledAsciArray.join("");
+  const finalstring = randomString.join("").concat(shuffledAsciArray.join(""));
 
   return finalstring;
 };
@@ -49,3 +54,17 @@ export const generatePassword = (config) => {
   const fullstring = generatePasswordString(config);
   return fullstring.substring(0, config.range);
 };
+
+
+export const strengthMeter = (config) => {
+    const selectedValues = Object.keys(config).map(val => config[val]);
+    const length = selectedValues.filter(value => value).length;
+    const conf = {
+        0: null,
+        1: "WEAK",
+        2: "FAIR",
+        3: "GOOD",
+        4: "STRONG"
+    }
+    return conf[length];
+}
